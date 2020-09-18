6ca52423ff31266a55191d00831af1dce49a3a28
Robert Smith <robertsmith@Roberts-MacBook-Pro.local>
2020-09-18 15:38:10 +0100

    added copying buffer example

--- buffer.js
+++ buffer.js
@@ -19,3 +19,10 @@ console.log( buf.toString('ascii', 0, 5)); //outputs abcde
 console.log( buf.toString('utf8', 0, 5)); //outputs abcde
 console.log( buf.toString(undefined, 0, 5)); //encoding defaults to utf8
 // outputs abcde
+
+var buffer1 = new Buffer.alloc(3, 'ABC');
+//copy a buffer
+var buffer2 = new Buffer.alloc(3);
+console.log("buffer 2 content: " + buffer2.toString());
+buffer1.copy(buffer2);
+console.log("buffer 2 content: " + buffer2.toString());
